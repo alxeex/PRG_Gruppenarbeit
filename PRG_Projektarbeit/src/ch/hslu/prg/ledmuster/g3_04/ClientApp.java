32093b878f5be6956ba5068d47aab62f8ce6a494
alxeex <ziegleralex72@gmail.com>
2024-11-01 15:04:24 +0100

    Commit Test Andri

--- PRG_Projektarbeit/src/ch/hslu/prg/ledmuster/g3_04/ClientApp.java
+++ PRG_Projektarbeit/src/ch/hslu/prg/ledmuster/g3_04/ClientApp.java
@@ -7,10 +7,8 @@ import ch.hslu.prg.ledboard.proxy.LedColor;
 import ch.hslu.prg.ledboard.proxy.Led;
 
 public class ClientApp {
-    //Hallo Alex
-    //BliblaBlub
-	//Test
-	
+    
+	//Test2
 	//TestTest
 	private static final int DEFAULT_PAUSE_TIME = 100;  // Zeit in Millisekunden zwischen Ein- und Ausschaltaktionen
 
